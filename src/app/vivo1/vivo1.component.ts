7d00231f42a531fa75ccbd4061f25706f0d6b6fb
Aryan Jyoti <aryanjyoti@L-W1K2RY0QG5.local>
2023-03-24 14:18:28 +0530

    add-to-cart3

--- src/app/vivo1/vivo1.component.ts
+++ src/app/vivo1/vivo1.component.ts
@@ -7,9 +7,11 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./vivo1.component.css']
 })
 export class Vivo1Component implements OnInit {
+  timeLeft: number = 300;
   key1:any
   device:any
   photos:any[]=[]
+  timeLeftString: string | undefined;
   constructor(private route:ActivatedRoute){ }
 
   name = 'Angular';
@@ -116,6 +118,26 @@ export class Vivo1Component implements OnInit {
 
   ngOnInit(): void {
 
+    setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+        const minutes = Math.floor(this.timeLeft / 60);
+        const seconds = this.timeLeft % 60;
+        this.timeLeftString = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      }
+    },1000)
+
+
+    // setTimeout(() => {
+    //   alert('Welcome Bajaj Mall!');
+    // }, 20000);
+
+    // setInterval(() => {
+    //   if(this.timeLeft > 0) {
+    //     this.timeLeft--;
+    //   }
+    // },1000)
+
     this.key1=this.route.snapshot.paramMap.get("id1")
     console.log('this is aryan',this.key1)
     if(this.key1==1){
@@ -166,17 +188,17 @@ export class Vivo1Component implements OnInit {
   }
 
 
-  device_id=[{name:'Vvivo V23 5G 128 GB Storage Sunshine Gold (8 GB RAM)',price:29990,storage1:'128',storage2:'256',processor:'Mediatek Dimensity 920',camera1:'64 MP + 8 MP + 2 MP', camera2:'50 MP + 8 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'vivo Y75 (4G) 128 GB Storage Moonlight Shadow (8 GB RAM)',price:19999,storage1:'128',storage2:'256',processor:'MediaTek Helio G96 Octa Core',camera1:'50 MP + 8 MP + 2 MP', camera2:'44 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'vivo X80 128 GB Storage Cosmic Black (8 GB RAM)',price:54999,storage1:'128',storage2:'256',processor:'MediaTek Dimensity 9000',camera1:'50 MP + 12 MP + 12 MP', camera2:'32 MP', Display:'6.78 inch',battery:'4500 mAh'},
-             {name:'vivo T1 Pro 5G 128 GB Storage Turbo Cyan (8 GB RAM)',price:24999,storage1:'128',storage2:'256',processor:'Snapdragon 695',camera1:'50 MP + 2 MP + 2 MP', camera2:'16 MP', Display:'6.43 inch',battery:'4700 mAh'},
-             {name:'vivo Y01 32 GB Storage Sapphire Blue (2 GB RAM)',price:12999,storage1:'128',storage2:'256',processor:'Mediatek Helio P35',camera1:'8 MP', camera2:'5 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'i phone 13',price:62790,storage1:'128',storage2:'256',processor:'Apple A15 Bionic (5 nm)',camera1:'12 MP f/1.6 26mm (wide)', camera2:'12 MP', Display:'6.1 inches',battery:'3240 mAh'},
-             {name:'i phone 14',price:72999,storage1:'128',storage2:'256',processor:'Apple A15 Bionic',camera1:'12 MP f/1.6 26mm (wide)', camera2:'12 MP', Display:'6.1 inches',battery:'3279 mAh'},
-             {name:'i phone 12 pro max',price:113990,storage1:'128',storage2:'256',processor:'Apple A14 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'i phone 13 pro max',price:113990,storage1:'128',storage2:'256',processor:'Apple A15 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'i phone 14 pro max',price:136590,storage1:'128',storage2:'256',processor:'Apple A16 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
-             {name:'ASUS rog phone 6 pro 5G',price:89990,storage1:'128',storage2:'256',processor:'snapdragon 8gen1',camera1:'64 MP + 2 MP + 2 MP', camera2:'34 MP', Display:'6.43 inch',battery:'4500 mAh'},
+  device_id=[{im:'vivo2',name:'Vvivo V23 5G 128 GB Storage Sunshine Gold (8 GB RAM)',price:29990,storage1:'128',storage2:'256',processor:'Mediatek Dimensity 920',camera1:'64 MP + 8 MP + 2 MP', camera2:'50 MP + 8 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'vivo4',name:'vivo Y75 (4G) 128 GB Storage Moonlight Shadow (8 GB RAM)',price:19999,storage1:'128',storage2:'256',processor:'MediaTek Helio G96 Octa Core',camera1:'50 MP + 8 MP + 2 MP', camera2:'44 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'vivo6',name:'vivo X80 128 GB Storage Cosmic Black (8 GB RAM)',price:54999,storage1:'128',storage2:'256',processor:'MediaTek Dimensity 9000',camera1:'50 MP + 12 MP + 12 MP', camera2:'32 MP', Display:'6.78 inch',battery:'4500 mAh'},
+             {im:'vivo7',name:'vivo T1 Pro 5G 128 GB Storage Turbo Cyan (8 GB RAM)',price:24999,storage1:'128',storage2:'256',processor:'Snapdragon 695',camera1:'50 MP + 2 MP + 2 MP', camera2:'16 MP', Display:'6.43 inch',battery:'4700 mAh'},
+             {im:'vivo8',name:'vivo Y01 32 GB Storage Sapphire Blue (2 GB RAM)',price:12999,storage1:'128',storage2:'256',processor:'Mediatek Helio P35',camera1:'8 MP', camera2:'5 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'iphone13',name:'i phone 13',price:62790,storage1:'128',storage2:'256',processor:'Apple A15 Bionic (5 nm)',camera1:'12 MP f/1.6 26mm (wide)', camera2:'12 MP', Display:'6.1 inches',battery:'3240 mAh'},
+             {im:'iphone14',name:'i phone 14',price:72999,storage1:'128',storage2:'256',processor:'Apple A15 Bionic',camera1:'12 MP f/1.6 26mm (wide)', camera2:'12 MP', Display:'6.1 inches',battery:'3279 mAh'},
+             {im:'iphone12promax',name:'i phone 12 pro max',price:113990,storage1:'128',storage2:'256',processor:'Apple A14 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'iphone13promax',name:'i phone 13 pro max',price:113990,storage1:'128',storage2:'256',processor:'Apple A15 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'iphone14promax',name:'i phone 14 pro max',price:136590,storage1:'128',storage2:'256',processor:'Apple A16 Bionic (5 nm)',camera1:'64 MP + 2 MP + 2 MP', camera2:'12 MP', Display:'6.43 inch',battery:'4500 mAh'},
+             {im:'rog1',name:'ASUS rog phone 6 pro 5G',price:89990,storage1:'128',storage2:'256',processor:'snapdragon 8gen1',camera1:'64 MP + 2 MP + 2 MP', camera2:'34 MP', Display:'6.43 inch',battery:'4500 mAh'},
             ]
 
 
