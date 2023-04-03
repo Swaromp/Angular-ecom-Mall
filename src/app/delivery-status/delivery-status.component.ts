5ed645ecc15557de06047ed2c2ed77d3661e3abc
Aryan Jyoti <aryanjyoti@L-W1K2RY0QG5.local>
2023-04-03 17:26:30 +0530

    delivery status

--- src/app/delivery-status/delivery-status.component.ts
+++ src/app/delivery-status/delivery-status.component.ts
@@ -7,17 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryStatusComponent implements OnInit {
   data:any;
+  deliverySteps = ['Order Placed', 'Shipped', 'Out for Delivery', 'Delivered'];
+  activeStep = 0;
+  thankYouMessage = 'Thank you for shopping!';
 
 
   constructor() { }
 
+
   ngOnInit(): void {
     this.get();
   }
+
+
   get() {
     var data=localStorage.getItem('dev_info')
+
+
     this.data = JSON.parse(localStorage.getItem('dev_info') || '{}');
+  }
+  setActiveStep(index: number) {
+    this.activeStep = index;
+  }
+
+  goToNextStep() {
+    if (this.activeStep < this.deliverySteps.length - 1) {
+      this.activeStep++;
+    }
   }
 
 
 }
+
+
+
