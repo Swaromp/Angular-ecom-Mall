ecbdcf5f01fd4e65ce0ec0b33de759319846662c
swarom <swarom8@gmail.com>
2023-04-11 12:16:37 +0530

    commonBranchNoLOgin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { WishlistComponent } from './wishlist/wishlist.component';
 import { SMenuComponent } from './smenu/smenu.component';
 // import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './home/carousel/carousel.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Vivo1Component } from './vivo1/vivo1.component';
-
+import { KartComponent } from './kart/kart.component';
+import { ComparemobileComponent } from './home/comparemobile/comparemobile.component';
+import { AddToKartComponent } from './add-to-kart/add-to-kart.component';
 const routes: Routes = [
   { path: '', component: CarouselComponent},
   { path:'smenu/:id', component: SMenuComponent},
